8f114f78a2dba2495fc01c4ed7699c46cf6eb30f
joycewangui <joycewangui590@gmail.com>
2024-10-12 10:05:49 +0300

    many to many

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,10 +1,60 @@
-class Author:
-    pass
+# lib/authors.py
+
+
+# lib/authors.py
+
+# lib/authors.py
 
 
 class Book:
-    pass
+    def __init__(self, title):
+        self.title = title
+
+    def contracts(self):
+        return [contract for contract in Contract.all if contract.book == self]
+
+    def authors(self):
+        return [contract.author for contract in self.contracts()]
+
+
+class Author:
+    def __init__(self, name):
+        self.name = name
+
+    def contracts(self):
+        return [contract for contract in Contract.all if contract.author == self]
+
+    def books(self):
+        return [contract.book for contract in self.contracts()]
+
+    def sign_contract(self, book, date, royalties):
+        return Contract(self, book, date, royalties)
+
+    def total_royalties(self):
+        return sum(contract.royalties for contract in self.contracts())
 
 
 class Contract:
-    pass
+    all = []  # Class variable to store all contract instances
+
+    def __init__(self, author, book, date, royalties):
+        # Validate inputs
+        if not isinstance(author, Author):
+            raise Exception("Author must be an instance of Author")
+        if not isinstance(book, Book):
+            raise Exception("Book must be an instance of Book")
+        if not isinstance(date, str):
+            raise Exception("Date must be a string")
+        if not isinstance(royalties, int):
+            raise Exception("Royalties must be an integer")
+
+        self.author = author
+        self.book = book
+        self.date = date
+        self.royalties = royalties
+        Contract.all.append(self)  # Add this contract to the class variable
+
+    @classmethod
+    def contracts_by_date(cls, date):
+        # Returns a list of contracts that match the specified date
+        return [contract for contract in cls.all if contract.date == date]
